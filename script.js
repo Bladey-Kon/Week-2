1abb39e73959515b27eddf680e89a89d7bfae445
Bladey-Kon <92902276+Bladey-Kon@users.noreply.github.com>
2021-11-17 21:07:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Hello\nWorld");
 console.log(3 * 8 + 2);
 console.log( 5 < 10);
 console.log(10 > 5);
-console.log("Apples" == "Oranges");
+console.log("Apples" != "Oranges");
 console.log(10 * 10 > 50 && 5 * 5 < 50);
 console.log(1 + 2 > 5 || 1 * 10 > 5);
 console.log(2021 * null);
